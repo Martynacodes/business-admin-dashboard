2e0f3182fe804f1e53337e34b543aab682fda896
Martynacodes <martyna.salome@gmail.com>
2023-07-06 23:41:57 -0400

    Add mock data for the app.

--- server/index.js
+++ server/index.js
@@ -30,3 +30,10 @@ app.use("/sales", salesRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(()=>{
+    app.listen(PORT, () =>console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`));
